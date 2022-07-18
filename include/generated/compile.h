856955d816eac3f6c0f6bcc6e90dd33ba451ab0f
gzg <1576310566@qq.com>
2022-07-17 23:03:55 -0700

    3 commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 6 23:27:22 PDT 2022"
+#define LINUX_COMPILE_BY "gzg64"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.2 20110630 (prerelease) (Freescale MAD -- Linaro 2011.07 -- Built at 2011/08/10 09:20)"
